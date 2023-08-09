0b2c0deebb977c0afc3f003c081fd8906fefebd3
BrunoPavanelli <brunopavanellicontato@gmail.com>
2023-08-09 17:01:12 -0300

    feat(tasks): implement task abstract repository class

---
+++ src/tasks/repositories/typeorm/typeorm.tasks.repository.ts
@@ -0,0 +1,13 @@
+import { Task } from "../../../database/entities/tasks.entity";
+import { TTaskRequest, TTaskUpdate } from "../../interfaces/tasks.interfaces";
+
+abstract class TasksRepositorie {
+    abstract create(taskData: TTaskRequest): Promise<Task>;
+    abstract findAll(): Promise<Task[]>;
+    abstract findById(taskId: string): Promise<Task | null>;
+    abstract findByName(taskName: string): Promise<Task>;
+    abstract updateById(taskId: string, taskData: TTaskUpdate): Promise<Task>;
+    abstract deleteById(taskId: string): Promise<void>;    
+}
+
+export { TasksRepositorie }
