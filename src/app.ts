a70a09c7f95886b19933fb77b7322301c575d5a7
BrunoPavanelli <brunopavanellicontato@gmail.com>
2023-08-09 19:50:46 -0300

    feat(permissions): implement routes in app

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,13 @@ import "express-async-errors"
 
 import { usersRoute } from './users/routes/users.routes';
 import { tasksRoute } from './tasks/routes/tasks.routes';
+import { permissionsRoute } from './permissions/routes/permissions.routes';
 
 const app = express()
 app.use(json())
 
 app.use("/users", usersRoute);
 app.use("/tasks", tasksRoute);
+app.use("/permissions", permissionsRoute);
 
 export default app
