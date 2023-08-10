97dcff57508b8e026426f8f329c6b127b8915fee
BrunoPavanelli <brunopavanellicontato@gmail.com>
2023-08-09 22:18:26 -0300

    feat(tasks_dealine): implement schemas

--- src/tasks/schemas/tasks.schemas.ts
+++ src/tasks/schemas/tasks.schemas.ts
@@ -11,6 +11,20 @@ const request = task.omit({ id: true, created_at: true });
 
 const update = request.partial();
 
-const schemas = { task, request, update };
+const dateRegex = new RegExp(/\d\d\/\d\d\/\d\d\d\d/i);
+const hourRegex = new RegExp(/\d\d:\d\d:\d\d/i);
+
+const deadline = z.object({
+    id: z.string(),
+    expiration_data: z.string().regex(dateRegex),
+    expiration_hour: z.string().regex(hourRegex),
+    created_at: z.date()
+})
+
+const deadlineRequest = deadline.omit({ id: true, created_at: true });
+
+const deadlineUpdate = deadlineRequest.partial();
+
+const schemas = { task, request, update, deadline, deadlineRequest, deadlineUpdate };
 
 export { schemas };
